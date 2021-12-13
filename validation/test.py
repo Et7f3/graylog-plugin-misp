ea9b22cc9d875b80c3d70feb82e51f346bc1764a
c8y3 <25362953+c8y3@users.noreply.github.com>
2021-12-13 13:23:55 +0100

    Factored test infrastructure so that everything is available on object GraylogServer

--- validation/test.py
+++ validation/test.py
@@ -10,19 +10,13 @@
 
 from unittest import TestCase
 from graylog_server import GraylogServer
-from graylog_rest_api import GraylogRestApi
-
-_PERIOD = 5
 
 
 class Test(TestCase):
 
     def setUp(self) -> None:
-        # TODO maybe merge _graylog and _graylog_rest_api
         self._graylog = GraylogServer('../runtime')
         self._graylog.start()
-        self._graylog_rest_api = GraylogRestApi()
-        self._graylog_rest_api.wait_until_graylog_has_started()
 
     def tearDown(self) -> None:
         self._graylog.stop()
@@ -31,12 +25,5 @@ class Test(TestCase):
         logs = self._graylog.extract_logs()
         self.assertIn('INFO : org.graylog2.bootstrap.CmdLineTool - Loaded plugin: MISP', logs)
 
-    def _has_pipeline_rule_function(self, name):
-        pipeline_rule_functions = self._graylog_rest_api.get('system/pipelines/rule/functions')
-        for pipeline_rule_function in pipeline_rule_functions.json():
-            if pipeline_rule_function['name'] == name:
-                return True
-        return False
-
     def test_pipeline_rule_functions_should_include_misp_lookup(self):
-        self.assertTrue(self._has_pipeline_rule_function('misp_lookup'))
+        self.assertTrue(self._graylog.has_pipeline_rule_function('misp_lookup'))
